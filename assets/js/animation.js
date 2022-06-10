4e85b857d05d57413004b1868bf6f2fe4e2fa815
Rupert Parry <rp@rupert.cloud>
2022-06-10 16:53:56 +1000

    Added styling and globe icon animation

---
+++ assets/js/animation.js
@@ -0,0 +1,10 @@
+const icon = document.querySelector('#globe-icon')
+
+let index = 0
+let emojis = ['🌏', '🌎', '🌍']
+
+setInterval(() => {
+  icon.innerText = emojis[index]
+  index += 1
+  if (index >= emojis.length) index = 0
+}, 800)
